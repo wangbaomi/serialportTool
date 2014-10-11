a6f1f465350d6e8b056bf18a33423d5dbe53d4ac
wbm <system1029@gmail.com>
2014-10-11 18:07:43 +0800

    add tool

---
+++ 串口测试工具/Cmd.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+
+
+namespace 串口测试工具
+{
+    public partial class Form1 : Form
+    {
+        private static string result_filename = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + "-result.txt";
+        private static FileStream result_file = new FileStream(result_filename, FileMode.Create);//保存自动测试的结果
+        private StreamWriter result_sw = new StreamWriter(result_file);
+
+        private void HandleCmd(string cmd)
+        {
+            //LogHelper.WriteDebug(typeof(MainForm), "接收指令: " + cmd);
+            //如果收到USE XML XXX的消息，就加载这个配置文件，并打开串口, 然后发送指令 USE OK
+            if (cmd.Contains("USE XML"))
+            {
+                string file = cmd.Split(default(Char[]), 3, StringSplitOptions.RemoveEmptyEntries)[2];
+                result_sw.WriteLine("配置文件：" + file);
+                //LoadConfig(file);
+                //this.richTextBox2.Text = "USE OK";
+                //SendMsgButton_Click(null, null);
+            }
+            //如果收到USE OK的消息，说明对方已经加载好配置文件，并已经打开了串口；
+            //这边就可以发字符串了，然后发送指令 SEND OK
+            else if (cmd.Contains("USE OK"))
+            {
+                //startSendButton_Click(null, null);
+                //this.richTextBox2.Text = "SEND OK";
+                //SendMsgButton_Click(null, null);
+            }
+            //如果收到SEND OK消息，说明对方已经发送了字符串，这边可以接收并检查接收的字符串了，
+            //然后发送字符串到串口，最后发送 SEND OVER指令，表明双方都发送完毕
+            else if (cmd.Contains("SEND OK"))
+            {
+                //bool ret = check();
+                //startSendButton_Click(null, null);
+                //this.richTextBox2.Text = "SEND OVER";
+                //SendMsgButton_Click(null, null);
+            }
+            //如果收到 SEND OVER消息，说明己方和对方都已发送了串口消息，一个配置文件测试完毕
+            else if (cmd.Contains("SEND OVER"))
+            {
+                //bool ret = check();
+                //this.richTextBox1.Text = ("OVER");
+            }
+            //自动测试完毕
+            else if (cmd.Contains("AutoTest Over"))
+            {
+                result_sw.Flush();
+                result_sw.Close();
+                result_file.Close();
+            }
+            else
+            {
+                //LogHelper.WriteDebug(typeof(MainForm), "接收指令: " + cmd + ", 非法指令!");
+            }
+
+        }
+    }
+}
---
+++ 串口测试工具/TCP.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace 串口测试工具
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener local_listener;//监听本地端口
+        private TcpClient remoteClient;//用于获取对方发来的消息
+        private NetworkStream streamToClient;//用于读取对方发来的消息
+        private TcpClient client;//TCP客户端,用于向对方发送消息
+        private void StartServer()
+        {
+
+            try
+            {
+                IPAddress.Parse(this.myIP.Text);
+                Int32.Parse(this.myPort.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return;
+            }
+            const int BufferSize = 1024;
+
+            IPAddress ip = IPAddress.Parse(this.myIP.Text);
+            Int32 port = Int32.Parse(this.myPort.Text);
+
+            try
+            {
+                local_listener = new TcpListener(ip, port);
+                local_listener.Start();           // 开始侦听
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+            this.startListen.Enabled = false;
+
+
+
+            // 获取一个连接，中断方法
+            remoteClient = local_listener.AcceptTcpClient();
+
+            // 获得流，并写入buffer中
+            streamToClient = remoteClient.GetStream();
+            try
+            {
+                do
+                {
+                    byte[] buffer = new byte[BufferSize];
+                    int bytesRead = streamToClient.Read(buffer, 0, BufferSize);
+
+                    // 获得请求的字符串
+                    string msg = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                    this.hisMsg.Text = msg;
+
+                    //HandleCmd(msg);
+                } while (true);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                streamToClient.Close();
+                remoteClient.Close();
+                local_listener.Stop();
+                return;
+            }
+        }
+
+
+        private void StartClient()
+        {
+            try
+            {
+                IPAddress.Parse(this.hisIP.Text);
+                Int32.Parse(this.hisPort.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return;
+            }
+
+            if (client == null)
+            {
+                IPAddress ip = IPAddress.Parse(this.hisIP.Text);
+                Int32 port = Int32.Parse(this.hisPort.Text);
+                try
+                {
+                    client = new TcpClient();
+                    client.Connect(ip, port);      // 与服务器连接
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+
+            string msg = this.myMsg.Text;
+            NetworkStream streamToServer = client.GetStream();
+            try
+            {
+                byte[] buffer = Encoding.Unicode.GetBytes(msg);     // 获得缓存
+                streamToServer.Write(buffer, 0, buffer.Length);     // 发往服务器
+                //LogHelper.WriteDebug(typeof(MainForm), "发送指令: " + msg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                streamToServer.Close();
+                client.Close();
+                //LogHelper.WriteDebug(typeof(MainForm), "发送指令: " + msg + " fail, error = " + ex.Message);
+                return;
+            }
+
+        }
+
+        
+    }
+}
